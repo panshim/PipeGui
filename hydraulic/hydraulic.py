1725285e432825ba6812a5d170c11e4f4175296c
shimin <panshimin0903@outlook.com>
2024-03-23 16:56:58 +0800

    进一步整理

---
+++ hydraulic/hydraulic.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------------------------
+# Hydraulic Components Copyright 2024-2024 by pan shimin & gao jingru
+# ----------------------------------------------------------------------------
+class AbstractComponent:
+    def __init__(self):
+        return
+
+
+class Actuator(AbstractComponent):
+    def __init__(self):
+        return
+
+
+class Pump(AbstractComponent):
+        def __init__(self):
+        return
